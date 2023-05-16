5ccc7f81fcc09b241bae75856cf227a6170f9840
Andres Jose Paternina Rubiano <apaterninar@gmail.com>
2023-05-16 01:10:08 -0500

    h

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -18,6 +18,10 @@ export default function Home(){
     const indexOfFirstPokemon=indexOfLastPokemon - pokemonsPerPage
     const currentPokemons= allPokemons.slice(indexOfFirstPokemon, indexOfLastPokemon);
     const [name, setName]=useState("")
+    const [searchError, setSearchError] = useState(false); 
+    const [selectedSort, setSelectedSort] = useState(""); // Estado del filtro de ordenamiento
+    const [selectedFilterType, setSelectedFilterType] = useState("All"); // Estado del filtro de tipos
+    const [selectedFilterCreated, setSelectedFilterCreated] = useState("All"); // Estado del filtro de creación
     const paginado =(pageNumber)=>{
         setCurrentPage(pageNumber)
     }
@@ -31,57 +35,67 @@ export default function Home(){
         e.preventDefault();
         setLoading(true);
         dispatch(getPokemons())
-        .then(() => setLoading(false));
-    };
+        .then(() => {setLoading(false)
+        setOrden(""); 
+      setCurrentPage(1); 
+      setName(""); 
+      setSearchError(false); 
+      setSelectedSort(""); // Restablecer el filtro de ordenamiento
+      setSelectedFilterType("All"); // Restablecer el filtro de tipos
+      setSelectedFilterCreated("All"); 
+    });
+}
     const handleSort =(e)=>{
         e.preventDefault();
         dispatch(orderByName(e.target.value))
         setCurrentPage(1);
-        setOrden(`Ordenado ${e.target.value}`)
+        setSelectedSort(e.target.value);
     };
     const handleSortAttack =(e)=>{
         e.preventDefault();
         dispatch(orderByAttack(e.target.value))
         setCurrentPage(1);
-        setOrden(`Ordenado ${e.target.value}`)
+        setSelectedSort(e.target.value);
     };
     const handleSortDefense =(e)=>{
         e.preventDefault();
         dispatch(orderByDefense(e.target.value))
         setCurrentPage(1);
-        setOrden(`Ordenado ${e.target.value}`)
+        setSelectedSort(e.target.value);
     };
     const handleSortLife =(e)=>{
         e.preventDefault();
         dispatch(orderByLife(e.target.value))
         setCurrentPage(1);
-        setOrden(`Ordenado ${e.target.value}`)
+        setSelectedSort(e.target.value);
     };
     const handleSortWeight =(e)=>{
         e.preventDefault();
         dispatch(orderByWeight(e.target.value))
         setCurrentPage(1);
-        setOrden(`Ordenado ${e.target.value}`)
+        setSelectedSort(e.target.value);
     };
     const handleSortHeight =(e)=>{
         e.preventDefault();
         dispatch(orderByHeight(e.target.value))
         setCurrentPage(1);
-        setOrden(`Ordenado ${e.target.value}`)
+        setSelectedSort(e.target.value);
     };
     const handleSortSpeed =(e)=>{
         e.preventDefault();
         dispatch(orderBySpeed(e.target.value))
         setCurrentPage(1);
-        setOrden(`Ordenado ${e.target.value}`)
+        setSelectedSort(e.target.value);
     };
     const handleFilterTypes=(e)=>{
         e.preventDefault();
         dispatch(filterPokemonsByTypes(e.target.value))
+        setSelectedFilterType(e.target.value);
     };
     const handleFilterCreated=(e)=>{
         e.preventDefault();
         dispatch(filterCreated(e.target.value))
+        setSelectedFilterCreated(e.target.value);
     };    
     function handleInputChange(e){
         e.preventDefault()
@@ -92,7 +106,13 @@ export default function Home(){
         e.preventDefault();
         setLoading(true);
         dispatch(getNamePokemons(name))
-        .then(() => setLoading(false));
+        .then(() => {setLoading(false);
+        setSearchError(false) // Si la búsqueda tiene éxito, se restablece el estado de error
+        })
+      .catch(() => {
+        setLoading(false);
+        setSearchError(true); // Si ocurre un error en la búsqueda, se establece el estado de error a true
+      });
     }
     
     return (
@@ -106,53 +126,53 @@ export default function Home(){
         <button className={style.button2} onClick={e=>{handleClick(e)}}>Clear filters</button>
         </div>
         <div >
-        <input type="text" placeholder='Search...' onChange={(e)=>handleInputChange(e)}></input>
+        <input value={name} type="text" placeholder='Search...' onChange={(e)=>handleInputChange(e)}></input>
             <button  type="submit" onClick={(e)=>handleSubmit(e)} >Search</button>
         </div>
         <div >
-        <select onChange={e=>handleSort(e)} className={style.order}>
+        <select value={selectedSort} onChange={e=>handleSort(e)} className={style.order}>
             <option>---</option>
             <option value="asc" >A-Z</option>
             <option value="desc">Z-A</option>
         </select>
-        <select onChange={e=>handleSortLife(e)} className={style.order}>
+        <select  value={selectedSort} onChange={e=>handleSortLife(e)} className={style.order}>
             <option>---</option>
             <option value="asc life" >-life</option>
             <option value="desc life">+life</option>
         </select>
         </div>
         <div>
-        <select onChange={e=>handleSortAttack(e)} className={style.order}>
+        <select  value={selectedSort} onChange={e=>handleSortAttack(e)} className={style.order}>
             <option>---</option>
             <option value="asc attack">-Attack</option>
             <option value="desc attack">+ Attack</option>
         </select>
-        <select onChange={e=>handleSortDefense(e)} className={style.order}>
+        <select  value={selectedSort} onChange={e=>handleSortDefense(e)} className={style.order}>
             <option>---</option>
             <option value="asc defense">-Defense</option>
             <option value="desc defense">+ Defense</option>
         </select>
         </div>
         <div>
-        <select onChange={e=>handleSortWeight(e)} className={style.order}>
+        <select  value={selectedSort} onChange={e=>handleSortWeight(e)} className={style.order}>
             <option>---</option>
             <option value="asc weight">-Weight</option>
             <option value="desc weight">+ weight</option>
         </select>
-        <select onChange={e=>handleSortHeight(e)} className={style.order}>
+        <select  value={selectedSort} onChange={e=>handleSortHeight(e)} className={style.order}>
             <option>---</option>
             <option value="asc height">-height</option>
             <option value="desc height">+ height</option>
         </select>
         </div>
         <div >
-        <select onChange={e=>handleFilterCreated(e)} className={style.filter}>
-            <option value='All'>---</option>
+        <select  value={selectedFilterCreated} onChange={e=>handleFilterCreated(e)} className={style.filter}>
+            <option value='All'>All</option>
             <option value="created">Created</option>
             <option value="api">From Api</option>
         </select>
-        <select onChange={e=>handleFilterTypes(e)} className={style.filter}>
-            <option value='All'>---</option>
+        <select value={selectedFilterType} onChange={e=>handleFilterTypes(e)} className={style.filter}>
+            <option value='All'>All</option>
             <option value="normal">normal</option>
             <option value="fighting">fighting</option>
             <option value="flying">flying</option>
@@ -176,7 +196,7 @@ export default function Home(){
         </select>
         </div>
         <div>
-        <select onChange={e=>handleSortSpeed(e)} className={style.order}>
+        <select value={selectedSort} onChange={e=>handleSortSpeed(e)} className={style.order}>
             <option>---</option>
             <option value="asc speed">-Speed</option>
             <option value="desc speed">+ Speed</option>
@@ -200,20 +220,28 @@ export default function Home(){
         </div> 
         <div className={style.columnsR}>
             {
-                 loading ?
+                 loading ?(
                 <div className={style.img2}>
                 <img  src="https://media.tenor.com/74l5y1hUdtwAAAAj/pokemon.gif"
                 width="250px" height="250px" />
                 <p>Loading...</p>
                </div>
-               :
+                 ): searchError ? (
+                    <div>
+                         <img className={style.img3} src="https://i0.wp.com/eltallerdehector.com/wp-content/uploads/2022/06/6420b-pikachu-sentado-png.png?resize=800%2C800&ssl=1"
+                width="150px" height="150px" />
+                <div className={style.errorMsg}>
+                    <h1>Error 404</h1>
+                    <h5>The pokemon does not exist</h5></div>
+                    </div>
+                 ):(
             currentPokemons.map((pok)=>{
                 return (
                 <div className={style.cardContainer} >
                 <Card name={pok.name} Types={pok.Types} image={pok.img?pok.img:pok.image} id={pok.id} key={pok.id}/> 
                 </div>
                 )
-            })
+            }))
             }
         </div>
         </div>
