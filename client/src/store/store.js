6d45787d2c4181f0c2d130ef5cadc3ea7b066df8
Andres Jose Paternina Rubiano <apaterninar@gmail.com>
2023-06-08 17:07:19 -0500

    angelita

--- client/src/store/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore, applyMiddleware} from "redux";
-import {composeWithDevTools} from 'redux-devtools-extension'
-import rootReducer from "../reducer/reducer";
-import thunk from "redux-thunk";
-
-export const store=createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
